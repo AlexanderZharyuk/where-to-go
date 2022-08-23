5466088a14333081e8653d5690824010075e466d
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-08-23 17:25:39 +0300

    Get places from db

--- places/views.py
+++ places/views.py
@@ -2,36 +2,31 @@ from django.shortcuts import render
 
 from django.templatetags.static import static
 
+from .models import Place
+
 
 def show_index_page(request):
-    places = {
-      "type": "FeatureCollection",
-      "features": [
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.62, 55.793676]
-          },
-          "properties": {
-            "title": "«Легенды Москвы",
-            "placeId": "moscow_legends",
-            "detailsUrl": static('places/moscow_legends.json')
-          }
-        },
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.64, 55.753676]
-          },
-          "properties": {
-            "title": "Крыши24.рф",
-            "placeId": "roofs24",
-            "detailsUrl": static('places/roofs24.json')
-          }
+    places = Place.objects.all()
+    features = []
+
+    for place in places:
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.latitude, place.longitude]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": static('places/moscow_legends.json')
+            }
         }
-      ]
+        features.append(feature)
+
+    places = {
+        "type": "FeatureCollection",
+        "features": features
     }
 
     data = {'places': places}
