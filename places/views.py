6d60df1e9cc88d2f577a43111770ff093907ffd4
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-08-23 18:18:48 +0300

    Add API-endpoint for json-response to user

--- places/views.py
+++ places/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 from django.templatetags.static import static
+from django.http import HttpResponse, JsonResponse
 
-from .models import Place
+from .models import Place, Image
 
 
 def show_index_page(request):
@@ -31,3 +31,27 @@ def show_index_page(request):
 
     data = {'places': places}
     return render(request, 'index.html', context=data)
+
+
+def get_place(request, place_id):
+    place = get_object_or_404(Place, pk=place_id)
+    images_urls = [image.images.url for image in place.images.all()]
+    json_response = {
+        'title': place.title,
+        'imgs': images_urls,
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lng': place.longitude,
+            'lat': place.latitude
+        }
+    }
+
+    return JsonResponse(
+        json_response,
+        safe=False,
+        json_dumps_params={
+            'ensure_ascii': False,
+            'indent': 4
+        }
+    )
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from places.views import show_index_page
+from places.views import show_index_page, get_place
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', show_index_page)
+    path('', show_index_page),
+    path('places/<int:place_id>/', get_place)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
