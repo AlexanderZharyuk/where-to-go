23eb79326dd5771dfaa791f9bcdd904d33b68e79
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-08-23 17:11:22 +0300

    Move data from template to view

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import show_index_page
+from places.views import show_index_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
