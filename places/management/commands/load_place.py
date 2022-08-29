f1e89c7f4486962af81a12d54de7129c7235d6f9
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-08-29 17:34:17 +0300

    Split function

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -9,6 +9,33 @@ from django.core.exceptions import MultipleObjectsReturned
 from places.models import Place, Image
 
 
+def create_new_place(parsed_place: dict):
+    return Place.objects.get_or_create(
+        title=parsed_place['title'],
+        defaults={
+            'description_short': parsed_place.setdefault(
+                'description_short', ''),
+            'description_long': parsed_place.setdefault(
+                'description_long', ''),
+            'longitude': parsed_place['coordinates']['lng'],
+            'latitude': parsed_place['coordinates']['lat']
+        },
+    )
+
+
+def save_place_images(place, image_url):
+        response = requests.get(image_url)
+        response.raise_for_status()
+
+        image_name = os.path.split(image_url)[-1]
+        image = ContentFile(response.content, name=image_name)
+
+        Image.objects.create(
+            place=place,
+            image=image
+        )
+
+
 class Command(BaseCommand):
     help = 'Загружает новое место из json-файла в БД'
 
@@ -21,17 +48,7 @@ class Command(BaseCommand):
 
         parsed_place = response.json()
         try:
-            place, created = Place.objects.get_or_create(
-                title=parsed_place['title'],
-                defaults={
-                    'description_short': parsed_place.setdefault(
-                        'description_short', ''),
-                    'description_long': parsed_place.setdefault(
-                        'description_long', ''),
-                    'longitude': parsed_place['coordinates']['lng'],
-                    'latitude': parsed_place['coordinates']['lat']
-                },
-            )
+            place, created = create_new_place(parsed_place)
         except MultipleObjectsReturned:
             self.stdout.write(
                 self.style.WARNING(
@@ -42,17 +59,8 @@ class Command(BaseCommand):
 
         if created:
             for image_url in parsed_place['imgs']:
-                response = requests.get(image_url)
-                response.raise_for_status()
-
-                image_name = os.path.split(image_url)[-1]
-                image = ContentFile(response.content, name=image_name)
-
-                Image.objects.create(
-                    place=place,
-                    image=image
-                )
-
+                save_place_images(place, image_url)
+                
             self.stdout.write(
                 self.style.SUCCESS(
                     f'Место {parsed_place["title"]} успешно занесено в БД.'
